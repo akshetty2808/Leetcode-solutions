5458f38eb008883b1ed9f609eb75b527b9d95103
Akshath C Shetty <93930947+akshetty2808@users.noreply.github.com>
2023-03-24 13:56:29 +0530

    Add files via upload

---
+++ Sqrt(x).py
@@ -0,0 +1,10 @@
+import math
+class Solution(object):
+    def mySqrt(self, x):
+        res = math.sqrt(x)
+
+        return int(res)
+        """
+        :type x: int
+        :rtype: int
+        """
